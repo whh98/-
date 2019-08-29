e367fd3360b323f0ae3fecb6f36e9ede8e0d887c
hehe <hehe98431@outlook.com>
2019-08-29 18:59:49 +0800

    文章修改轮播图添加网站设置添加

--- alibaixiu/public/assets/js/common.js
+++ alibaixiu/public/assets/js/common.js
@@ -1,16 +1,26 @@
-$('#logout').on('click', function () {
+$('#logout').on('click', function() {
     var isConfirm = confirm('您真的要退出吗?');
     if (isConfirm) {
-      // alert('用户点击了确认按钮')
-      $.ajax({
-        type: 'post',
-        url: '/logout',
-        success: function () {
-          location.href = 'login.html';
-        },
-        error: function () {
-          alert('退出失败')
-        }
-      })
+        // alert('用户点击了确认按钮')
+        $.ajax({
+            type: 'post',
+            url: '/logout',
+            success: function() {
+                location.href = 'login.html';
+            },
+            error: function() {
+                alert('退出失败')
+            }
+        })
     }
-  });
+});
+
+$.ajax({
+    type: 'get',
+    url: '/users/' + userId,
+    success: function(res) {
+        // console.log(res);
+        $('.avatar').attr('src', res.avatar);
+        $('.name').html(res.nickName);
+    }
+})
